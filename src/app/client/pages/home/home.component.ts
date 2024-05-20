78cf492d6c4bd2fbe65be286a4f31e164e26e67b
domvega99 <davega12.dv@gmail.com>
2024-05-20 22:30:01 +0800

    client side updates

--- src/app/client/pages/home/home.component.ts
+++ src/app/client/pages/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { TeamsComponent } from '../../components/teams/teams.component';
+import { MatButton } from '@angular/material/button';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [TeamsComponent],
+  imports: [TeamsComponent, MatButton],
   templateUrl: './home.component.html',
   styleUrl: './home.component.sass'
 })

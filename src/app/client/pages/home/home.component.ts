e460fada6240fb64d31b943bdae8e5fb01247610
jordanako2 <@christopherjordanolmoguez@gmail.com>
2024-06-10 14:56:18 +0800

    menu fix

--- src/app/client/pages/home/home.component.ts
+++ src/app/client/pages/home/home.component.ts
@@ -7,7 +7,6 @@ import { MatTableModule } from '@angular/material/table';
 import { MatIconModule } from '@angular/material/icon';
 import { MatCardModule } from '@angular/material/card';
 import {MatTabsModule} from '@angular/material/tabs';
-import { LeftsideContentComponent } from "./leftside-content/leftside-content.component";
 
 
 export interface PeriodicElement {
@@ -46,7 +45,7 @@ const ELEMENT_DATA: PeriodicElement[] = [
     standalone: true,
     templateUrl: './home.component.html',
     styleUrl: './home.component.sass',
-    imports: [TeamsComponent, MatButton, LeftContentComponent, RightContentComponent, MatTableModule, MatIconModule, MatCardModule, MatTabsModule, LeftsideContentComponent]
+    imports: [TeamsComponent, MatButton, LeftContentComponent, RightContentComponent, MatTableModule, MatIconModule, MatCardModule, MatTabsModule,]
 })
 export class HomeComponent {
 

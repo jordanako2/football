a3fe64fd5cda6a893c100c5bcce43787e0707ca3
domvega99 <davega12.dv@gmail.com>
2024-06-12 23:33:31 +0800

    updates

--- src/app/admin/components/sidebar/sidebar.component.ts
+++ src/app/admin/components/sidebar/sidebar.component.ts
@@ -2,14 +2,54 @@ import { Component, ElementRef, Renderer2 } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
 import { MatListModule } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
+import { CommonModule } from '@angular/common';
+
+interface MenuItem {
+  label: string;
+  icon: string;
+  routerLink: string;
+}
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [ RouterModule, MatListModule, MatIconModule ],
+  imports: [ RouterModule, MatListModule, MatIconModule, CommonModule],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.sass'
 })
 export class SidebarComponent {
+
+    menuItems: MenuItem[] = [
+    {
+      label: 'Dashboard',
+      icon: 'dashboard',
+      routerLink: '/admin/dashboard'
+    },
+    {
+      label: 'Post',
+      icon: 'store',
+      routerLink: '/admin/posts'
+    },
+    {
+      label: 'Teams',
+      icon: 'store',
+      routerLink: '/admin/teams'
+    },
+    {
+      label: 'Leagues',
+      icon: 'store',
+      routerLink: '/admin/leagues'
+    },
+    {
+      label: 'News',
+      icon: 'store',
+      routerLink: '/admin/news'
+    },
+    {
+      label: 'Users',
+      icon: 'group',
+      routerLink: '/admin/users'
+    }
+  ];
 
 }

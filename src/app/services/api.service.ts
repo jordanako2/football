ad6fdba576866cfb5738d90e0570cdaebe741c50
domvega99 <davega12.dv@gmail.com>
2024-06-17 08:10:18 +0800

    updates

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -5,5 +5,5 @@ import { environment } from '../environments/environment';
   providedIn: 'root'
 })
 export class ApiService {
-  public readonly URL_IMAGE = `https:florify.online/uploads/`;
+  public readonly URL_IMAGE = `https://florify.online/uploads/`;
 }

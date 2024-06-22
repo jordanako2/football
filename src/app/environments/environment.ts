1c1bb1e43decff007c10d4a8a21649c6b3fc37b0
Dominic Anuta Vega <davega12.dv@gmail.com>
2024-06-22 08:51:10 +0800

    merge doom

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:3000',
+    apiUrl: 'https://florify.online',
     googleClientId: '657320183260-g9pe0d04ggrkjn1edct4res49fb223o9.apps.googleusercontent.com',
     facebookAppId: '708976131282089'
 };

4aea35f9e562245e91a0907aa50db30e781902d5
domvega99 <davega12.dv@gmail.com>
2024-07-03 00:01:35 +0800

    environments

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,13 +1,13 @@
-// export const environment = {
-//     production: false,
-//     apiUrl: 'https://apii.florify.online',
-//     googleClientId: '657320183260-g9pe0d04ggrkjn1edct4res49fb223o9.apps.googleusercontent.com',
-//     facebookAppId: '708976131282089'
-// };
-
 export const environment = {
     production: false,
-    apiUrl: 'https://api.bfl.ph',
+    apiUrl: 'https://apii.florify.online',
     googleClientId: '657320183260-g9pe0d04ggrkjn1edct4res49fb223o9.apps.googleusercontent.com',
     facebookAppId: '708976131282089'
 };
+
+// export const environment = {
+//     production: false,
+//     apiUrl: 'https://api.bfl.ph',
+//     googleClientId: '657320183260-g9pe0d04ggrkjn1edct4res49fb223o9.apps.googleusercontent.com',
+//     facebookAppId: '708976131282089'
+// };

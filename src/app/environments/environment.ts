0dcbba3f5cfda5d7b4823ce4f3a0e485d5c0e71e
Dominic Anuta Vega <davega12.dv@gmail.com>
2024-07-03 08:44:49 +0800

    environments

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,13 +1,23 @@
+// FOR DEVELOPMENT
 export const environment = {
     production: false,
     apiUrl: 'https://apii.florify.online',
     googleClientId: '657320183260-g9pe0d04ggrkjn1edct4res49fb223o9.apps.googleusercontent.com',
-    facebookAppId: '708976131282089'
+    facebookAppId: '708976131282089',
+    imageUrl: 'https://apii.florify.online/uploads/',
+    imageContentUrl: 'https://apii.florify.online/uploads/content/'
 };
 
+
+
+
+
+// FOR PRODUCTION
 // export const environment = {
 //     production: false,
 //     apiUrl: 'https://api.bfl.ph',
 //     googleClientId: '657320183260-g9pe0d04ggrkjn1edct4res49fb223o9.apps.googleusercontent.com',
 //     facebookAppId: '708976131282089'
+//     imageUrl: 'https://api.bfl.ph/uploads/',
+//     imageContentUrl: 'https://api.bfl.ph/uploads/content/'
 // };
--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -5,9 +5,8 @@ import { environment } from '../environments/environment';
   providedIn: 'root'
 })
 export class ApiService {
-  public readonly URL_IMAGE = `https://apii.florify.online/uploads/`;
-  public readonly URL_CONTENT_IMAGE = `https://apii.florify.online/uploads/content/`;
 
-  // public readonly URL_IMAGE = `https://api.bfl.ph/uploads/`;
-  // public readonly URL_CONTENT_IMAGE = `https://api.bfl.ph/uploads/content/`;
+  public readonly URL_IMAGE = environment.imageUrl;
+  public readonly URL_CONTENT_IMAGE = environment.imageContentUrl;
+
 }

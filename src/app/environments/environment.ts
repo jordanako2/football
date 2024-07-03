699bf7c5d13789ff23dcd60ad2b85559f237c875
Dominic Anuta Vega <davega12.dv@gmail.com>
2024-07-03 08:50:29 +0800

    local environment

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,15 +1,12 @@
-// FOR DEVELOPMENT
-export const environment = {
-    production: false,
-    apiUrl: 'https://apii.florify.online',
-    googleClientId: '657320183260-g9pe0d04ggrkjn1edct4res49fb223o9.apps.googleusercontent.com',
-    facebookAppId: '708976131282089',
-    imageUrl: 'https://apii.florify.online/uploads/',
-    imageContentUrl: 'https://apii.florify.online/uploads/content/'
-};
-
-
-
+// // FOR DEVELOPMENT
+// export const environment = {
+//     production: false,
+//     apiUrl: 'https://apii.florify.online',
+//     googleClientId: '657320183260-g9pe0d04ggrkjn1edct4res49fb223o9.apps.googleusercontent.com',
+//     facebookAppId: '708976131282089',
+//     imageUrl: 'https://apii.florify.online/uploads/',
+//     imageContentUrl: 'https://apii.florify.online/uploads/content/'
+// };
 
 
 // FOR PRODUCTION
@@ -21,3 +18,14 @@ export const environment = {
 //     imageUrl: 'https://api.bfl.ph/uploads/',
 //     imageContentUrl: 'https://api.bfl.ph/uploads/content/'
 // };
+
+
+// FOR LOCAL
+export const environment = {
+    production: false,
+    apiUrl: 'http://localhost:3000',
+    googleClientId: '657320183260-g9pe0d04ggrkjn1edct4res49fb223o9.apps.googleusercontent.com',
+    facebookAppId: '708976131282089',
+    imageUrl: 'http://localhost:3000/uploads/',
+    imageContentUrl: 'http://localhost:3000/uploads/content/'
+};

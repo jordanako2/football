8a32573b92548eeef398cb8cf78a10d1f6dfb74c
Dominic Anuta Vega <davega12.dv@gmail.com>
2024-07-15 10:44:34 +0800

    club content

--- src/app/client/components/overview/overview.component.ts
+++ src/app/client/components/overview/overview.component.ts
@@ -7,6 +7,8 @@ import { QuillModule } from 'ngx-quill';
 import { TeamAboutService } from '../../../services/team-about.service';
 import { ActivatedRoute } from '@angular/router';
 import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
+import { ContentsService } from '../../../services/contents.service';
+import { ApiService } from '../../../services/api.service';
 
 @Component({
   selector: 'app-overview',
@@ -19,19 +21,25 @@ export class OverviewComponent {
 
   teamId: number | null = null;
   aboutData: any | null = null;
+  contentData: any[] | null = null;
   content: SafeHtml  | null = null;
+  imagePath: string | null = null;
 
   constructor (
     private route: ActivatedRoute,
     private teamAboutService: TeamAboutService,
+    private contentService: ContentsService,
+    private _configService: ApiService,
     private sanitizer: DomSanitizer
   ) {}
 
   ngOnInit(): void {
+    this.imagePath = `${this._configService.URL_CONTENT_IMAGE}`;
     this.route.params.subscribe(params => {
       this.teamId = +params['id'];
       if (this.teamId) {
         this.getTeamAboutByTeamId(this.teamId);
+        this.getContentByTeamId(this.teamId);
       }
     });
   }
@@ -44,6 +52,17 @@ export class OverviewComponent {
       },
       error: (err) => {
         console.log(err);
+      }
+    })
+  }
+
+  getContentByTeamId(teamId: number) {
+    this.contentService.getContentByTeamId(teamId).subscribe({
+      next: (res) => {
+        this.contentData = res
+      },
+      error: (err) => {
+        console.log(err)
       }
     })
   }

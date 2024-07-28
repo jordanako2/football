f102fca8a5a5e6468553d202d7e8442943ded3c6
domvega99 <davega12.dv@gmail.com>
2024-07-28 22:01:14 +0800

    token and refresh token

--- src/app/interceptor/auth.interceptor.ts
+++ src/app/interceptor/auth.interceptor.ts
@@ -1,15 +1,82 @@
-import { HttpInterceptorFn } from '@angular/common/http';
+import { HttpInterceptorFn, HttpRequest, HttpHandlerFn, HttpEvent, HttpErrorResponse } from '@angular/common/http';
 import { inject } from '@angular/core';
+import { Observable, throwError } from 'rxjs';
+import { catchError, switchMap } from 'rxjs/operators';
 import { AuthService } from '../services/auth.service';
+import { CookieService } from 'ngx-cookie-service';
+import { environment } from '../environments/environment';
 
-
-export const authInterceptor: HttpInterceptorFn = (req, next) => {
+export const authInterceptor: HttpInterceptorFn = (req: HttpRequest<any>, next: HttpHandlerFn): Observable<HttpEvent<any>> => {
   const authService = inject(AuthService);
-  const authToken = authService.getToken();
+  const cookieService = inject(CookieService);
+  const authToken = cookieService.get('accessToken');
 
-  const authReq = req.clone({
-    headers: req.headers.set('Authorization', `Bearer ${authToken}`)
-  });
+  if (authToken) {
+    req = req.clone({
+      headers: req.headers.set('Authorization', `Bearer ${authToken}`)
+    });
+  }
 
-  return next(authReq);
+  return next(req).pipe(
+    catchError((error: HttpErrorResponse) => {
+      if (error.status === 401 && !req.url.includes(environment.apiUrl+'/auth/refresh')) {
+        return authService.refreshToken().pipe(
+          switchMap(() => {
+            const newAccessToken = cookieService.get('accessToken');
+            if (newAccessToken) {
+              const authReq = req.clone({
+                headers: req.headers.set('Authorization', `Bearer ${newAccessToken}`)
+              });
+              return next(authReq);
+            }
+            return throwError(() => new Error('Failed to refresh token'));
+          }),
+          catchError((err) => {
+            authService.logout();
+            return throwError(() => new Error('Failed to refresh token and logout'));
+          })
+        );
+      }
+      return throwError(() => new Error(error.message));
+    })
+  );
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import { HttpInterceptorFn } from '@angular/common/http';
+// import { inject } from '@angular/core';
+// import { AuthService } from '../services/auth.service';
+
+
+// export const authInterceptor: HttpInterceptorFn = (req, next) => {
+//   const authService = inject(AuthService);
+//   const authToken = authService.getToken();
+
+//   const authReq = req.clone({
+//     headers: req.headers.set('Authorization', `Bearer ${authToken}`)
+//   });
+
+//   return next(authReq);
+// };
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Router } from '@angular/router';
-import { BehaviorSubject, Observable } from 'rxjs';
-import { tap } from 'rxjs/operators';
+import { BehaviorSubject, Observable, throwError } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
 import { CookieService } from 'ngx-cookie-service';
 import { jwtDecode } from 'jwt-decode';
 import { map } from 'rxjs/operators';
@@ -13,6 +13,7 @@ import { environment } from '../environments/environment';
 })
 export class AuthService {
   private apiUrl = environment.apiUrl+'/auth/login';
+  private apiRefreshUrl = environment.apiUrl+'/auth/refresh';
   private userSubject = new BehaviorSubject<any>(null);
   public user$ = this.userSubject.asObservable();
   private isAuthenticatedSubject = new BehaviorSubject<boolean>(this.isAuthenticated());
@@ -24,18 +25,52 @@ export class AuthService {
   ) {}
 
   login(email: string, password: string): Observable<void> {
-    return this.http.post<{ accessToken: string }>(this.apiUrl, { email, password })
+    return this.http.post<{ accessToken: string, refreshToken: string }>(this.apiUrl, { email, password })
       .pipe(
         map(response => {
-          const token = response.accessToken;
-          this.cookieService.set('key', token, { secure: true, sameSite: 'Strict', path: '/' });
-          const decodedToken = jwtDecode(token);
-          this.userSubject.next(decodedToken); 
-          this.isAuthenticatedSubject.next(true); 
+          const { accessToken, refreshToken } = response;
+          this.cookieService.set('key', accessToken, { secure: true, sameSite: 'Strict', path: '/' });
+          this.cookieService.set('refreshToken', refreshToken, { secure: true, sameSite: 'Strict', path: '/' });
+          const decodedToken = jwtDecode(accessToken);
+          this.userSubject.next(decodedToken);
+          this.isAuthenticatedSubject.next(true);
           this.router.navigate(['/']);
         })
       );
   }
+
+  refreshToken(): Observable<void> {
+    const refreshToken = this.cookieService.get('refreshToken');
+    return this.http.post<{ accessToken: string }>(this.apiRefreshUrl, { refreshToken })
+      .pipe(
+        map(response => {
+          const { accessToken } = response;
+          this.cookieService.set('key', accessToken, { secure: true, sameSite: 'Strict', path: '/' });
+          const decodedToken = jwtDecode(accessToken);
+          this.userSubject.next(decodedToken);
+        }),
+        catchError(error => {
+          this.logout();
+          return throwError(() => new Error(error.message));
+        })
+      );
+  }
+
+  // login(email: string, password: string): Observable<void> {
+  //   return this.http.post<{ accessToken: string }>(this.apiUrl, { email, password })
+  //     .pipe(
+  //       map(response => {
+  //         const token = response.accessToken;
+  //         this.cookieService.set('key', token, { secure: true, sameSite: 'Strict', path: '/' });
+  //         const decodedToken = jwtDecode(token);
+  //         this.userSubject.next(decodedToken); 
+  //         this.isAuthenticatedSubject.next(true); 
+  //         this.router.navigate(['/']);
+  //       })
+  //     );
+  // }
+
+
 
   loginfb(): void {
     window.location.href = environment.apiUrl+'/auth/facebook';
@@ -68,18 +103,12 @@ export class AuthService {
     this.http.get(environment.apiUrl + '/auth/logout')
       .subscribe({
         next: () => {
-          console.log('Logout successful, deleting cookies...');
-          
-          // Ensure all relevant cookies are deleted
-          this.cookieService.delete('key', '/'); // Adjust the domain if needed
-          console.log('Cookie deleted:', this.cookieService.get('key') === '');
-
-          // Reset user and authentication state
+          this.cookieService.delete('key', '/');
+          this.cookieService.delete('refreshToken', '/');
           this.userSubject.next(null);
           this.isAuthenticatedSubject.next(false);
-
-          console.log('User and authentication state reset.');
           this.router.navigate(['/login']);
+          console.log('Logout successful, deleting cookies...');
         },
         error: (error) => {
           console.error('Error during logout:', error);

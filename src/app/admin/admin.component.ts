84003f5007041764d85c054d8c58d6be8c46ede4
domvega99 <davega12.dv@gmail.com>
2024-10-10 22:52:07 +0800

    loading page

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -4,14 +4,23 @@ import { RouterOutlet } from '@angular/router';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { LoadingComponent } from './components/loading/loading.component';
+import { LoadingService } from './components/loading/loading.service';
 
 @Component({
   selector: 'app-admin',
   standalone: true,
-  imports: [DashboardComponent, RouterOutlet, MatSidenavModule, NavbarComponent, SidebarComponent],
+  imports: [DashboardComponent, RouterOutlet, MatSidenavModule, NavbarComponent, SidebarComponent, LoadingComponent],
   templateUrl: './admin.component.html',
   styleUrl: './admin.component.sass',
 })
 export class AdminComponent {
+  constructor(private loadingService: LoadingService) {}
 
+  ngOnInit() {
+    this.loadingService.show();
+    setTimeout(() => {
+      this.loadingService.hide();
+    }, 1000);
+  }
 }
---
+++ src/app/admin/components/loading/loading.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { LoadingService } from './loading.service';
+import { CommonModule } from '@angular/common';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-loading',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './loading.component.html',
+  styleUrl: './loading.component.sass'
+})
+export class LoadingComponent {
+  loading$: Observable<boolean>;
+
+  constructor(private loadingService: LoadingService) {
+    this.loading$ = this.loadingService.loading$;
+  }
+}

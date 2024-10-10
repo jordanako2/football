cf100659cf0377eb3a6549608c3c2d6ba6735f23
domvega99 <davega12.dv@gmail.com>
2024-10-10 21:05:17 +0800

    old updates

--- src/app/client/pages/home/home.component.ts
+++ src/app/client/pages/home/home.component.ts
@@ -6,8 +6,8 @@ import { RightContentComponent } from './right-content/right-content.component';
 import { MatTableModule } from '@angular/material/table';
 import { MatIconModule } from '@angular/material/icon';
 import { MatCardModule } from '@angular/material/card';
-import {MatTabsModule} from '@angular/material/tabs';
-import { Title } from '@angular/platform-browser';
+import { MatTabsModule } from '@angular/material/tabs';
+import { Title, Meta } from '@angular/platform-browser';
 
 
 @Component({
@@ -18,14 +18,13 @@ import { Title } from '@angular/platform-browser';
   imports: [TeamsComponent, MatButton, LeftContentComponent, RightContentComponent, MatTableModule, MatIconModule, MatCardModule, MatTabsModule,]
 })
 export class HomeComponent implements OnInit{
-  constructor(private _titleService: Title) {}
+  constructor(
+    private titleService: Title,
+    private metaService: Meta
+  ) {}
 
   ngOnInit(): void {
-    this.setTitle('Bohol Football League ');
+    this.titleService.setTitle("BFL - the Philippine's first grassroots and amateur football league.");
+    this.metaService.addTag({ name: 'description', content: "Bohol Football League representing the finest football clubs in the Philippine's for grassroots and amateur football. Keeping you up to date with match scores and club info." });
   }
-  
-  setTitle(newTitle: string) {
-    this._titleService.setTitle(newTitle);
-  }
-
 }

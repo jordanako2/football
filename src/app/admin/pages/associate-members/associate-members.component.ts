a42cb0eb4a7084aa51b60c8ec77b5c76042c2483
domvega99 <davega12.dv@gmail.com>
2024-10-11 00:17:10 +0800

    associate

---
+++ src/app/admin/pages/associate-members/associate-members.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-associate-members',
+  standalone: true,
+  imports: [],
+  templateUrl: './associate-members.component.html',
+  styleUrl: './associate-members.component.sass'
+})
+export class AssociateMembersComponent {
+
+}

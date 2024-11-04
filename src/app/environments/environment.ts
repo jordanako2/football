ea50bea4a40d999ac8f652ed2adf994cee36d44a
Unknown <unknown@example.com>
2024-11-04 21:11:00 +0800

    change the home meta The Philippines'

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,28 +1,28 @@
 // FOR DEVELOPMENT
-export const environment = {
-    production: false,
-    apiUrl: 'https://florify.online/',
-    googleClientId: '657320183260-g9pe0d04ggrkjn1edct4res49fb223o9.apps.googleusercontent.com',
-    facebookAppId: '708976131282089',
-    imageUrl: 'https://florify.online/uploads/',
-    imageContentUrl: 'https://florify.online/uploads/content/',
-    imageSquadUrl: 'https://florify.online/uploads/squad/',
-    imageLogo: 'https://florify.online/uploads/website/',
-    clientUrl: 'http://localhost:4200/',
-};
+// export const environment = {
+//     production: false,
+//     apiUrl: 'https://florify.online/',
+//     googleClientId: '657320183260-g9pe0d04ggrkjn1edct4res49fb223o9.apps.googleusercontent.com',
+//     facebookAppId: '708976131282089',
+//     imageUrl: 'https://florify.online/uploads/',
+//     imageContentUrl: 'https://florify.online/uploads/content/',
+//     imageSquadUrl: 'https://florify.online/uploads/squad/',
+//     imageLogo: 'https://florify.online/uploads/website/',
+//     clientUrl: 'http://localhost:4200/',
+// };
 
 
 // FOR PRODUCTION
-// export const environment = {
-//     production: false,
-//     apiUrl: 'https://api.bfl.ph/',
-//     googleClientId: '657320183260-g9pe0d04ggrkjn1edct4res49fb223o9.apps.googleusercontent.com',
-//     facebookAppId: '708976131282089',
-//     imageUrl: 'https://api.bfl.ph/uploads/',
-//     imageContentUrl: 'https://api.bfl.ph/uploads/content/',
-//     imageSquadUrl: 'https://api.bfl.ph/uploads/squad/',
-//     imageLogo: 'https://api.bfl.ph/uploads/website/',
-// };
+export const environment = {
+    production: false,
+    apiUrl: 'https://api.bfl.ph/',
+    googleClientId: '657320183260-g9pe0d04ggrkjn1edct4res49fb223o9.apps.googleusercontent.com',
+    facebookAppId: '708976131282089',
+    imageUrl: 'https://api.bfl.ph/uploads/',
+    imageContentUrl: 'https://api.bfl.ph/uploads/content/',
+    imageSquadUrl: 'https://api.bfl.ph/uploads/squad/',
+    imageLogo: 'https://api.bfl.ph/uploads/website/',
+};
 
 
 // FOR LOCAL

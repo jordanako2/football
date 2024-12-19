1a23cbcbbab6b589c63a9e0c2c832f5c578fd6ef
jordanako2 <@christopherjordanolmoguez@gmail.com>
2024-12-19 15:07:48 +0800

    clubs locator features

--- src/app/client/pages/home/home.component.ts
+++ src/app/client/pages/home/home.component.ts
@@ -6,7 +6,7 @@ import { RightContentComponent } from './right-content/right-content.component';
 import { MatTableModule } from '@angular/material/table';
 import { MatIconModule } from '@angular/material/icon';
 import { MatCardModule } from '@angular/material/card';
-import { MatTabsModule } from '@angular/material/tabs';
+import {MatTabsModule} from '@angular/material/tabs';
 import { Title, Meta } from '@angular/platform-browser';
 
 
@@ -19,12 +19,20 @@ import { Title, Meta } from '@angular/platform-browser';
 })
 export class HomeComponent implements OnInit{
   constructor(
-    private titleService: Title,
-    private metaService: Meta
-  ) {}
+    private _titleService: Title,
+    private meta: Meta
+  ) {
+    this.meta.addTag({name: 'description', content: 'Bohol Football League representing the finest football clubs in the Philippines for grassroots and amateur football. Keeping you up to date with match scores and club info.'});
+    this.meta.addTag({name: 'keywords', content:'Bohol Football League, Bohol Football'});
+  }
 
   ngOnInit(): void {
-    this.titleService.setTitle("BFL - The Philippines' first grassroots and amateur football league.");
-    this.metaService.addTag({ name: 'description', content: "Bohol Football League representing the finest football clubs in the Philippine's for grassroots and amateur football. Keeping you up to date with match scores and club info." });
+    this.setTitle('BFL - the Philippines first grassroots and amateur football league.');
+    
   }
+  
+  setTitle(newTitle: string) {
+    this._titleService.setTitle(newTitle);
+  }
+
 }
--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -13,26 +13,26 @@
 
 
 // FOR PRODUCTION
-// export const environment = {
-//     production: false,
-//     apiUrl: 'https://api.bfl.ph/',
-//     googleClientId: '657320183260-g9pe0d04ggrkjn1edct4res49fb223o9.apps.googleusercontent.com',
-//     facebookAppId: '708976131282089',
-//     imageUrl: 'https://api.bfl.ph/uploads/',
-//     imageContentUrl: 'https://api.bfl.ph/uploads/content/',
-//     imageSquadUrl: 'https://api.bfl.ph/uploads/squad/',
-//     imageLogo: 'https://api.bfl.ph/uploads/website/',
-// };
+export const environment = {
+    production: false,
+    apiUrl: 'https://api.bfl.ph/',
+    googleClientId: '657320183260-g9pe0d04ggrkjn1edct4res49fb223o9.apps.googleusercontent.com',
+    facebookAppId: '708976131282089',
+    imageUrl: 'https://api.bfl.ph/uploads/',
+    imageContentUrl: 'https://api.bfl.ph/uploads/content/',
+    imageSquadUrl: 'https://api.bfl.ph/uploads/squad/',
+    imageLogo: 'https://api.bfl.ph/uploads/website/',
+};
 
 
 // FOR LOCAL
-export const environment = {
-    production: false,
-    apiUrl: 'http://localhost:3000',
-    googleClientId: '657320183260-g9pe0d04ggrkjn1edct4res49fb223o9.apps.googleusercontent.com',
-    facebookAppId: '708976131282089',
-    imageUrl: 'http://localhost:3000/uploads/',
-    imageContentUrl: 'http://localhost:3000/uploads/content/',
-    imageSquadUrl: 'https://florify.online/uploads/squad/',
-    imageLogo: 'https://florify.online/uploads/website/',
-};
+// export const environment = {
+//     production: false,
+//     apiUrl: 'http://localhost:3000',
+//     googleClientId: '657320183260-g9pe0d04ggrkjn1edct4res49fb223o9.apps.googleusercontent.com',
+//     facebookAppId: '708976131282089',
+//     imageUrl: 'http://localhost:3000/uploads/',
+//     imageContentUrl: 'http://localhost:3000/uploads/content/',
+//     imageSquadUrl: 'https://florify.online/uploads/squad/',
+//     imageLogo: 'https://florify.online/uploads/website/',
+// };
